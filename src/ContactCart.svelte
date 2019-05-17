4976aea664f37100ef5d27e3b0e9892dabb8f327
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-17 18:39:01 +0300

    use dynamic classes

--- src/ContactCart.svelte
+++ src/ContactCart.svelte
@@ -25,6 +25,10 @@
     height: 100%;
   }
 
+  .thumb-placeholder {
+    background-color: tomato;
+  }
+
   img {
     width: 100%;
     height: 100%;
@@ -61,7 +65,7 @@
 
 <div class="contact-card">
   <header>
-    <div class="thumb">
+    <div class="thumb" class:thumb-placeholder={!imageUrl}>
       <img src={imageUrl} alt="" />
     </div>
     <div class="user-data">
