8771c99a43b4c943390355b8fb5efe130c178f83
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-17 17:48:23 +0300

    user reactive variables

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,14 @@
   let name = "sahil";
   let age = 30;
 
+  $: uppercaseName = name.toUpperCase();
+
   function incAge() {
     age += 1;
+  }
+
+  function changeName() {
+    name = "Not Sahil";
   }
 </script>
 
@@ -13,5 +19,6 @@
   }
 </style>
 
-<h1>Hello {name}!, I'm {age} years old</h1>
+<h1>Hello {uppercaseName}!, I'm {age} years old</h1>
 <button on:click={incAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
