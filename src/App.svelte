85cd23e8b6b81fa8ea6b800b3e3570a33080d43e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-17 17:56:04 +0300

    use bind:value

--- src/App.svelte
+++ src/App.svelte
@@ -14,9 +14,9 @@
     age += 1;
   }
 
-  function changeName(event) {
-    name = event.target.value;
-  }
+  // function changeName(event) {
+  //   name = event.target.value;
+  // }
 </script>
 
 <style>
@@ -28,4 +28,5 @@
 <h1>Hello {uppercaseName}!, I'm {age} years old</h1>
 <button on:click={incAge}>Change Age</button>
 <!-- <button on:click={changeName}>Change Name</button> -->
-<input type="text" value={name} on:input={changeName} />
+<!-- <input type="text" value={name} on:input={changeName} /> -->
+<input type="text" bind:value={name} />
