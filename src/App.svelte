700484594b58ec3b898dc2bfe87c2ebcd6623f6c
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-17 17:26:12 +0300

    basic string interpolation

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script>
-	export let name;
+  let name = "sahil";
+  let age = 30;
+
+  function incAge() {
+    age += 1;
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {name}!, I'm {age} years old</h1>
+<button on:click={incAge}>Change Age</button>
